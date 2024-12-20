800b27329314d9e288b3db1c27a68ec12fcb9361
GoodTY <taek3153@gmail.com>
2024-12-21 00:07:20 +0900

    결과 파일 존재시 덮어쓰기 활성화

--- extract_url_content.py
+++ extract_url_content.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import html2text
 from langdetect import detect
 import gc
+import os
 
 # CSV 파일 경로
 input_file = 'dataset/train_html.csv'
@@ -16,11 +17,11 @@ df = pd.read_csv(input_file, engine='python', on_bad_lines='skip')
 # 'Category' 열에서 'spam'은 1로, 'ham'은 0으로 변경
 df['Category'] = np.where(df['Category'] == 'spam', 1, 0)
 
-# 변경된 데이터를 새로운 CSV 파일로 저장
-df.to_csv(output_file, index=False)
+# 변경된 데이터를 새로운 CSV 파일로 저장 (덮어쓰기)
+df.to_csv(output_file, index=False, mode='w')
 
 # 데이터셋 로드
-df = pd.read_csv('dataset/ready_html.csv')
+df = pd.read_csv(output_file)
 
 # 'Category' 열을 'label'로 변경
 df.rename(columns={'Category': 'label'}, inplace=True)
@@ -86,9 +87,11 @@ df['extracted_text'] = df['Data'].apply(extract_text)
 # 영어 텍스트 필터링
 df = df[df['extracted_text'].apply(is_english_langdetect)]
 
-# CSV 형식으로 저장
+# 결과 CSV 파일 경로
 output_processed_file = 'dataset/ready_html_processed.csv'
-df.to_csv(output_processed_file, index=False)
+
+# CSV 파일 저장 (덮어쓰기)
+df.to_csv(output_processed_file, index=False, mode='w')
 
 # 메모리 정리
 del df
